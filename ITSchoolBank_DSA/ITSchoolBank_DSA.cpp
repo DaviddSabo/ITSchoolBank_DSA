003526fdaabc828a87fa0e3fe4958475f895a427
David Sabo <Sabo.david941@gmail.com>
2022-02-20 19:01:55 +0200

    Implementare meniu principal/ Implementation of main menu

--- ITSchoolBank_DSA/ITSchoolBank_DSA.cpp
+++ ITSchoolBank_DSA/ITSchoolBank_DSA.cpp
@@ -25,16 +25,51 @@ operatorul bancii sa introduca datele pentru crearea contului.(trebuie ca aceast
 
 int main()
 {
+  
     std::cout << "Alege una din urmatoarele optiuni\n";
+    std::cout << "\n";
+
     std::cout << "1-> Numar conturi\n";
     std::cout << "2-> Creare cont\n";
     std::cout << "3-> Modificare cont\n";
     std::cout << "4-> Stergere cont\n";
     std::cout << "5-> Eliberare\Depunere\n";
     std::cout << "6-> Detalii cont\n";
+    std::cout << "\n";
+
+    int optiune;
+    std::cout << "Selectati operatiunea aleasa\n";
+    std::cin >> optiune;
+    std::cout << "\n";
 
 
+    switch (optiune) {
+    case 1:
+        std::cout << "Ati ales optiunea 1\n";
+        break;
+    case 2:
+        std::cout << "Ati ales optiunea 2\n";
+        break;
 
+    case 3:
+        std::cout << "Ati ales optiunea 3\n";
+        break;
+
+    case 4:
+        std::cout << "Ati ales optiunea 4\n";
+        break;
+
+    case 5:
+        std::cout << "Ati ales optiunea 5\n";
+        break;
+
+    case 6:
+        std::cout << "Ati ales optiunea 6\n";
+        break;
+    default:
+        std::cout << "Optiune invalida\n";
+        break;
+    }
 
 
 }
